398cbaf7556032e54314ab8c0904e0de4b731ad1
Luca Eaton <eatonluca@gmail.com>
2026-04-05 00:54:25 -0400

    Created a method inorder to call python code within c++.
    We used the Python C lib in order to interpret the python.

---
+++ Gmail/cpp_email_classifier.h
@@ -0,0 +1,12 @@
+//
+// Created by luca eaton on 4/4/26.
+//
+
+#ifndef CPP_EMAIL_CLASSIFIER_H
+#define CPP_EMAIL_CLASSIFIER_H
+
+#include <string>
+
+std::string classify(const std::string& email);
+
+#endif
